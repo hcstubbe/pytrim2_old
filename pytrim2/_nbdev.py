7f64ee57991f5db6cb7cc097f6044ae939672f8a
hcstubbe <hstubbe@med.lmu.de>
2022-06-13 20:21:46 +0000

    added demultiplexing function

--- pytrim2/_nbdev.py
+++ pytrim2/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"findAlingments": "00_core.ipynb"}
+index = {"findAlingments": "00_core.ipynb",
+         "align_barcodes": "00_core.ipynb"}
 
 modules = ["core.py"]
 
